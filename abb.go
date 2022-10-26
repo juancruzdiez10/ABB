3a80fc2c6f2687193e9cd734865d584112f62da7
juancruzdiez10 <jdiez@fi.uba.ar>
2022-10-26 18:11:31 -0300

    cambios

--- abb.go
+++ abb.go
@@ -99,11 +99,15 @@ func (abb abb[K, V]) buscar(clave K, raiz *nodoAbb[K, V]) (*nodoAbb[K, V], *nodo
 	return nodo, padre
 }
 
-func (abb abb[K, V]) buscarMayor(nodo *nodoAbb[K, V]) *nodoAbb[K, V] {
+func (abb abb[K, V]) buscarMayor(nodo *nodoAbb[K, V], clave *K) *nodoAbb[K, V] {
 	if nodo.derecho == nil {
 		return nodo
+	} else if clave != nil {
+		if abb.cmp(nodo.derecho.clave, *clave) > 0 {
+			return nodo
+		}
 	}
-	return abb.buscarMayor(nodo.derecho)
+	return abb.buscarMayor(nodo.derecho, clave)
 }
 
 func (abb *abb[K, V]) Borrar(clave K) V {
@@ -113,15 +117,13 @@ func (abb *abb[K, V]) Borrar(clave K) V {
 	nodo, padre := abb.buscar(clave, abb.raiz)
 	clave_retornar := nodo.dato
 	if nodo.izquierdo != nil && nodo.derecho != nil {
-
-		reemplazo := abb.buscarMayor(nodo.izquierdo)
+		reemplazo := abb.buscarMayor(nodo.izquierdo, nil)
 		reemplazo_dato, reemplazo_clave := reemplazo.dato, reemplazo.clave
 		abb.Borrar(reemplazo.clave)
 		nodo.clave, nodo.dato = reemplazo_clave, reemplazo_dato
 		return clave_retornar //return aca para no restar la cantidad todavia
 
 	} else if padre == nil {
-
 		if nodo.izquierdo != nil {
 			abb.raiz = nodo.izquierdo
 		} else if nodo.derecho != nil {
@@ -131,15 +133,13 @@ func (abb *abb[K, V]) Borrar(clave K) V {
 		}
 
 	} else if nodo.izquierdo == nil && nodo.derecho == nil {
-
 		if abb.cmp(padre.clave, nodo.clave) > 0 {
 			padre.izquierdo = nil
 		} else {
 			padre.derecho = nil
 		}
 
-	} else if nodo.izquierdo == nil || nodo.derecho == nil { //un hijo, abuelo apunta al nieto
-
+	} else { //un hijo, abuelo apunta al nieto
 		var enlace **nodoAbb[K, V]
 		if abb.cmp(padre.clave, nodo.clave) > 0 {
 			enlace = &padre.izquierdo
@@ -150,70 +150,10 @@ func (abb *abb[K, V]) Borrar(clave K) V {
 			*enlace = nodo.izquierdo
 		} else {
 			*enlace = nodo.derecho
-		} //Creeria q este seria el unico para modificar, pero no encuentro manera mas corta que esta
-
+		}
 	}
 	abb.cantidad--
 	return clave_retornar
-}
-
-func (iter *iterAbb[K, V]) apilarHijos(nodo *nodoAbb[K, V], desde *K) {
-	if nodo == nil {
-		return
-	}
-	if iter.desde == nil {
-		iter.pila.Apilar(nodo)
-		iter.apilarHijos(nodo.izquierdo, desde)
-	} else {
-		if iter.cmp(nodo.clave, *desde) > 0 {
-			iter.pila.Apilar(nodo)
-			iter.apilarHijos(nodo.izquierdo, desde)
-		}
-	}
-
-}
-
-func (abb abb[K, V]) Iterador() IterDiccionario[K, V] {
-	//con recorrido preorder
-	iter := new(iterAbb[K, V])
-	pila := TDAPila.CrearPilaDinamica[*nodoAbb[K, V]]()
-	iter.pila = pila
-	iter.cmp = abb.cmp
-
-	if abb.raiz != nil {
-		iter.pila.Apilar(abb.raiz)
-		iter.apilarHijos(abb.raiz.izquierdo, iter.desde)
-	}
-
-	return iter
-}
-
-func (iter iterAbb[K, V]) HaySiguiente() bool {
-	//Verifico si la pila está vacía o el tope es mayor a hasta
-	if iter.hasta == nil {
-		return !iter.pila.EstaVacia()
-	} else {
-		return !iter.pila.EstaVacia() && iter.cmp(iter.pila.VerTope().clave, *iter.hasta) < 0
-	}
-}
-
-func (iter *iterAbb[K, V]) Siguiente() K {
-	if !iter.HaySiguiente() {
-		panic("El iterador termino de iterar")
-	}
-	elemento := iter.pila.Desapilar()
-	if elemento.derecho != nil {
-		iter.apilarHijos(elemento.derecho, iter.desde)
-	}
-	return elemento.clave
-}
-
-func (iter iterAbb[K, V]) VerActual() (K, V) {
-	if !iter.HaySiguiente() {
-		panic("El iterador termino de iterar")
-	}
-	elemento := iter.pila.VerTope()
-	return elemento.clave, elemento.dato
 }
 
 func (abb abb[K, V]) Iterar(visitar func(K, V) bool) {
@@ -225,21 +165,106 @@ func (nodo *nodoAbb[K, V]) iterar(visitar func(K, V) bool) {
 	if nodo == nil {
 		return
 	}
-
 	nodo.izquierdo.iterar(visitar)
 	if visitar(nodo.clave, nodo.dato) {
 		nodo.derecho.iterar(visitar)
 	}
 }
 
-func (abb abb[K, V]) IteradorRango(desde *K, hasta *K) IterDiccionario[K, V] {
+func (iter *iterAbb[K, V]) apilarHijos(nodo *nodoAbb[K, V], desde *K) {
+	if nodo == nil {
+		return
+	}
+	iter.pila.Apilar(nodo)
+	if iter.desde == nil {
+		iter.apilarHijos(nodo.izquierdo, desde)
+	} else {
+		if iter.cmp(nodo.clave, *desde) < 0 {
+			iter.Siguiente()
+		} else {
+			iter.apilarHijos(nodo.izquierdo, desde)
+		}
+	}
+}
+
+func (abb abb[K, V]) Iterador() IterDiccionario[K, V] {
+	//con recorrido preorder
 	iter := new(iterAbb[K, V])
-	pila := TDAPila.CrearPilaDinamica[*nodoAbb[K, V]]()
-	iter.pila = pila
-	iter.desde = desde
-	iter.hasta = hasta
+	iter.pila = TDAPila.CrearPilaDinamica[*nodoAbb[K, V]]()
+	iter.cmp = abb.cmp
+
 	if abb.raiz != nil {
 		iter.apilarHijos(abb.raiz, iter.desde)
 	}
+
 	return iter
+}
+
+func (iter iterAbb[K, V]) HaySiguiente() bool {
+	//Verifico si la pila está vacía o el tope es mayor a hasta
+	if iter.hasta != nil {
+		if !iter.pila.EstaVacia() {
+			return iter.cmp(iter.pila.VerTope().clave, *iter.hasta) <= 0
+		}
+	}
+	return !iter.pila.EstaVacia()
+}
+
+func (iter *iterAbb[K, V]) Siguiente() K {
+	if !iter.HaySiguiente() {
+		panic("El iterador termino de iterar")
+	}
+	elemento := iter.pila.Desapilar()
+	iter.apilarHijos(elemento.derecho, iter.desde)
+	return elemento.clave
+}
+
+func (iter iterAbb[K, V]) VerActual() (K, V) {
+	if !iter.HaySiguiente() {
+		panic("El iterador termino de iterar")
+	}
+	elemento := iter.pila.VerTope()
+	return elemento.clave, elemento.dato
+}
+
+func (abb abb[K, V]) elementoMedio(raiz *nodoAbb[K, V], desde K) *nodoAbb[K, V] {
+
+	if abb.cmp(raiz.clave, desde) < 0 {
+		return abb.elementoMedio(raiz.derecho, desde)
+	} else {
+		return raiz
+	}
+}
+
+func (abb abb[K, V]) IteradorRango(desde *K, hasta *K) IterDiccionario[K, V] {
+	iter := new(iterAbb[K, V])
+	iter.pila = TDAPila.CrearPilaDinamica[*nodoAbb[K, V]]()
+	iter.desde = desde
+	iter.hasta = hasta
+	if abb.cmp(*desde, *hasta) > 0 {
+		return iter
+	}
+	nodo := abb.elementoMedio(abb.raiz, *desde)
+	iter.apilarHijos(nodo, iter.desde)
+	return iter
+}
+
+func (abb abb[K, V]) IterarRango(desde *K, hasta *K, visitar func(K, V) bool) {
+	abb.raiz.iterarRango(desde, hasta, visitar, abb.cmp)
+}
+
+func (nodo *nodoAbb[K, V]) iterarRango(desde *K, hasta *K, visitar func(K, V) bool, cmp func(K, K) int) {
+	if nodo == nil || cmp(*desde, *hasta) > 0 {
+		return
+	}
+
+	if cmp(nodo.clave, *desde) >= 0 {
+		nodo.izquierdo.iterarRango(desde, hasta, visitar, cmp)
+	}
+	if cmp(nodo.clave, *desde) >= 0 && cmp(nodo.clave, *hasta) <= 0 {
+		visitar(nodo.clave, nodo.dato)
+	}
+	if cmp(nodo.clave, *hasta) <= 0 {
+		nodo.derecho.iterarRango(desde, hasta, visitar, cmp)
+	}
 }
